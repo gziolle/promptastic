0c441c6bc00f524dd85426a78125a4edb40492ed
Guilherme Ziolle <gziolle@gmail.com>
2019-02-21 17:36:46 -0300

    fix: made all constants final

--- app/src/main/java/com/gziolle/promptastic/util/Constants.java
+++ app/src/main/java/com/gziolle/promptastic/util/Constants.java
@@ -2,26 +2,28 @@ package com.gziolle.promptastic.util;
 
 public class Constants {
 
-    public static String KEY_TITLE = "title";
-    public static String KEY_CONTENT = "content";
-    public static String KEY_DATABASE_REF = "database_ref";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_CONTENT = "content";
+    public static final String KEY_DATABASE_REF = "database_ref";
 
-    public static String PATH_USERS = "users/";
-    public static String PATH_SCRIPTS = "/scripts";
+    public static final String PATH_USERS = "users/";
+    public static final String PATH_SCRIPTS = "/scripts";
 
-    public static int REQUEST_EDIT_SCRIPT = 101;
+    public static final int REQUEST_EDIT_SCRIPT = 101;
 
-    public static int COUNTDOWN_TOTAL_TIME = 3000;
-    public static int COUNTDOWN_TIME_INTERVAL = 1000;
-    public static String PLAY_SCRIPT_DEFAULT_TEXT_SIZE = "40";
+    public static final int COUNTDOWN_TOTAL_TIME = 3000;
+    public static final int COUNTDOWN_TIME_INTERVAL = 1000;
+    public static final String PLAY_SCRIPT_DEFAULT_TEXT_SIZE = "40";
 
-    public static String PLAY_SCRIPT_VERY_SLOW_DURATION = "200000";
-    public static String PLAY_SCRIPT_SLOW_DURATION = "150000";
-    public static String PLAY_SCRIPT_DEFAULT_DURATION = "100000";
-    public static String PLAY_SCRIPT_FAST_DURATION = "75000";
-    public static String PLAY_SCRIPT_VERY_FAST_DURATION = "50000";
+    public static final String PLAY_SCRIPT_VERY_SLOW_DURATION = "200000";
+    public static final String PLAY_SCRIPT_SLOW_DURATION = "150000";
+    public static final String PLAY_SCRIPT_DEFAULT_DURATION = "100000";
+    public static final String PLAY_SCRIPT_FAST_DURATION = "75000";
+    public static final String PLAY_SCRIPT_VERY_FAST_DURATION = "50000";
 
-    public static String PLAY_SCRIPT_THEME_LIGHT = "light";
-    public static String PLAY_SCRIPT_THEME_DARK = "dark";
+    public static final String PLAY_SCRIPT_THEME_LIGHT = "light";
+    public static final String PLAY_SCRIPT_THEME_DARK = "dark";
+
+    public static final int NAME_MAX_LENGTH = 60;
 
 }
